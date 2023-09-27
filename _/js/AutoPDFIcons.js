6181fef2cb8abdb88822f200d413cf0fca4b3a66
jeffreyfarnan <128228071+jeffreyfarnan@users.noreply.github.com>
2023-09-27 17:00:30 +0100

    Delete _ directory

--- _/js/AutoPDFIcons.js
+++
@@ -1,6 +0,0 @@
-$("document").ready(function() {
-        $("li a[href$=.email]").after("<img src='contactFolder/images/email.gif' align='absbottom' />");
-         $("li a[href$=.docx]").after("<img src='contactFolder/images/word.gif' align='absbottom' />");
-    });
-    
-    
